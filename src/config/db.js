3868013ff9d2e03ce42dd483b83ea2dddbd02ccf
Manuel.Millan1979 <nutriglar@gmail.com>
2025-12-31 11:20:33 -0300

    fix: configurar conexión MongoDB con dotenv

--- src/config/db.js
+++ src/config/db.js
@@ -2,4 +2,4 @@ import mongoose from 'mongoose';
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB conectado'))
-  .catch(err => console.error(err));
+  .catch(err => console.error('Error MongoDB:', err));
